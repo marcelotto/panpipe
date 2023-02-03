a6ecf557fcd7caee78c16532addcadb943f33c24
Marcel Otto <marcelotto@gmx.de>
2023-02-03 23:31:10 +0100

    Add test of new wikilinks extension

--- test/panpipe/panpipe_test.exs
+++ test/panpipe/panpipe_test.exs
@@ -2,4 +2,17 @@ defmodule PanpipeTest do
   use ExUnit.Case
   doctest Panpipe
 
+  test "wiki links" do
+    assert Panpipe.ast_fragment!("[[Foo]]", from: {:markdown, [:wikilinks_title_after_pipe]}) == %Panpipe.AST.Link{
+             children: [%Panpipe.AST.Str{string: "Foo"}],
+             target: "Foo",
+             title: "wikilink"
+           }
+
+    assert Panpipe.ast_fragment!("[[Foo|title]]", from: {:markdown, [:wikilinks_title_after_pipe]}) == %Panpipe.AST.Link{
+             children: [%Panpipe.AST.Str{string: "title"}],
+             target: "Foo",
+             title: "wikilink"
+           }
+  end
 end
