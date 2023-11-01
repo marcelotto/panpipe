363f44a21d5e9aa0e4fae00c378c65c84bd52d0c
Marcel Otto <marcelotto@gmx.de>
2023-11-01 21:37:34 +0100

    Remove unused dependencies

--- mix.exs
+++ mix.exs
@@ -60,7 +60,6 @@ defmodule Panpipe.MixProject do
 
       # Development
       {:stream_data, "~> 0.5", only: :test},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
